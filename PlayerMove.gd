2d72492fa97178d9796a3fa47e3a52f755e396cf
Erik Engström <wrikseng@gmail.com>
2023-10-04 13:57:44 +0200

    smooth walk

--- PlayerMove.gd
+++ PlayerMove.gd
@@ -8,7 +8,7 @@ const JUMP_VELOCITY = 4.5
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 
-func _physics_process(delta):
+func _process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
