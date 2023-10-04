23adcd3ee2c347bd239b7b1094ea5f03f3bf7f2c
Erik Engström <wrikseng@gmail.com>
2023-10-05 01:55:41 +0200

    anim start

--- Scripts/RotateCamera.gd
+++ Scripts/RotateCamera.gd
@@ -2,15 +2,24 @@ extends Node3D
 
 @export var sensitivity: float = 0.001
 @export var cam: Node3D
+@export var camWobble: Node3D
+@export var wobbleSensitivity: float = 1
+var boingo: float = 0
 
 func _init():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _process(delta):
+	#camWobble.rotate_z(event.relative.x * wobbleSensitivity)
+	pass
 
 func _unhandled_input(event):
 	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		rotate_y(-event.relative.x * sensitivity)
 		cam.rotate_x(-event.relative.y * sensitivity)
 		cam.rotation = Vector3(clamp(cam.rotation.x, deg_to_rad(-90), deg_to_rad(90)), 0, 0)
+		
+		#boingo = event.relative.x
 		
 		#print(Vector3(rad_to_deg(cam.rotation.x), rad_to_deg(cam.rotation.y), rad_to_deg(cam.rotation.z)))
 		#print(Vector3(cam.rotation.x, cam.rotation.y, cam.rotation.z))
