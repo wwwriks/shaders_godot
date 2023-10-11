86f8a8109abfd3792ecb26357e79af413795ef23
Erik Engström <wrikseng@gmail.com>
2023-10-11 19:50:29 +0200

    snog stuff

--- Scenes/SubViewport.gd
+++ Scenes/SubViewport.gd
@@ -3,15 +3,16 @@ extends SubViewport
 @export var player: Node3D
 @export var world_extents: Rect2
 @export var paint_brush : Sprite2D
+var player_body: CharacterBody3D
 
 func _ready():
-	pass
+	player_body = player.get_child(0)
 
-func _process(delta):
-	var half_world_extents = world_extents.size * 0.5;
-	var player_pos = Vector2(player.position.x, player.position.z)
+func _process(_delta):
+	var half_world_extents = world_extents.size * 0.5
+	var player_pos = Vector2(player_body.position.x, player_body.position.z)
 	
 	player_pos += half_world_extents
 	var paintbrush_position = player_pos / world_extents.size
-	
-	paint_brush.position = paint_brush.position * Vector2(size.x, size.y)
+
+	paint_brush.position = paintbrush_position * size.x
