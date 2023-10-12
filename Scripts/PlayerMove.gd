1a9fe656337340a2657cb573fcebab7b7437d1cb
Erik Engström <wrikseng@gmail.com>
2023-10-12 14:45:47 +0200

    fixes?

--- Scripts/PlayerMove.gd
+++ Scripts/PlayerMove.gd
@@ -10,8 +10,8 @@ func _process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
 
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (pivot_dir.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
